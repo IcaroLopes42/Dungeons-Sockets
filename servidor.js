6b38bdcb241145c827afb06c07feb471c5cf259f
IcaroLopes42 <icaro.santana@aluno.ufop.edu.br>
2026-02-26 23:48:03 -0300

    fix: adicionado um buffer para recebimento de mensagens via telnet

--- servidor.js
+++ servidor.js
@@ -3,73 +3,79 @@ const net = require('net');
 const jogadores = {};
 
 const servidor = net.createServer((socket) => {
-    let apelidoAtual = null; // Guarda quem é o dono desta conexão TCP
+    let apelidoAtual = null;
+    
+    let buffer = ''; 
+
     console.log(`[Nova Conexão FÍSICA]: ${socket.remoteAddress}`);
 
     socket.on('data', (data) => {
-        // O toString().trim() limpa a sujeira invisível do final da string
-        const mensagem = data.toString().trim();
-        console.log(`[Recebido de ${socket.remoteAddress}]: ${mensagem}`);
-        
-        
-        const partes = mensagem.split('|');
-        const comando = partes[0].toUpperCase();
+        buffer += data.toString(); 
 
-        switch (comando) {
-            case 'REGISTRAR':
-                // Exemplo de uso: REGISTRAR|Icaro|Bruxo
-                apelidoAtual = partes[1];
-                const classe = partes[2];
+        if (buffer.includes('\n')) {
+            
+            let mensagens = buffer.split('\n');
+            
+            buffer = mensagens.pop(); 
+
+            for (let msg of mensagens) {
+                const mensagemLimpa = msg.trim(); // Tira espaços e sujeiras
                 
-                // Define o HP base dependendo da classe escolhida
-                let hpBase = 20; // Padrão Ladino
-                if (classe === 'Clerigo') hpBase = 30;
-                if (classe === 'Bruxo') hpBase = 25;
+                if (mensagemLimpa === '') continue; // Ignora se o cara só apertou ENTER 
 
-                // Salva o jogador na memória do servidor
-                jogadores[apelidoAtual] = {
-                    socket: socket, // Guardamos o túnel TCP
-                    ip: socket.remoteAddress,
-                    classe: classe,
-                    hp: hpBase,
-                    status: 'livre'
-                };
+                console.log(`[Recebido Inteiro de ${socket.remoteAddress}]: ${mensagemLimpa}`);
                 
-                socket.write(`Servidor: Bem-vindo a Dungeons&Sockets, ${apelidoAtual} o ${classe}!\n`);
-                break;
+                const partes = mensagemLimpa.split('|');
+                const comando = partes[0].toUpperCase();
 
-            case 'LISTAR':
-                // Pega todos os nomes cadastrados
-                const online = Object.keys(jogadores);
-                if (online.length === 0) {
-                    socket.write(`Servidor: A Taverna esta vazia.\n`);
-                } else {
-                    socket.write(`Servidor: Jogadores online -> ${online.join(', ')}\n`);
+                switch (comando) {
+                    case 'REGISTRAR':
+                        apelidoAtual = partes[1];
+                        const classe = partes[2];
+                        
+                        let hpBase = 20; 
+                        if (classe === 'Clerigo') hpBase = 30;
+                        if (classe === 'Bruxo') hpBase = 25;
+
+                        jogadores[apelidoAtual] = {
+                            socket: socket,
+                            ip: socket.remoteAddress,
+                            classe: classe,
+                            hp: hpBase,
+                            status: 'livre'
+                        };
+                        
+                        socket.write(`Servidor: Bem-vindo a Dungeons&Sockets, ${apelidoAtual} o ${classe}!\n`);
+                        break;
+
+                    case 'LISTAR':
+                        const online = Object.keys(jogadores);
+                        if (online.length === 0) {
+                            socket.write(`Servidor: A Taverna esta vazia.\n`);
+                        } else {
+                            socket.write(`Servidor: Jogadores online -> ${online.join(', ')}\n`);
+                        }
+                        break;
+                    
+                    default:
+                        socket.write('Servidor: Comando desconhecido pelo Mestre da Mesa.\n');
                 }
-                break;
-            
-            // Aqui vão entrar os próximos comandos: DESAFIAR, ACEITAR, ACAO...
-            
-            default:
-                socket.write('Servidor: Comando desconhecido pelo Mestre da Mesa.\n');
+            }
         }
     });
 
-    // Quando o cliente fechar o terminal ou a internet cair:
     socket.on('end', () => {
         if (apelidoAtual && jogadores[apelidoAtual]) {
             console.log(`[Desconectou]: ${apelidoAtual} saiu da Taverna.`);
-            delete jogadores[apelidoAtual]; // Remove da memória
+            delete jogadores[apelidoAtual]; 
         }
     });
     
-    // Evita que o servidor crashe se der algum erro
     socket.on('error', (err) => {
         console.log(`Erro na conexão com ${apelidoAtual}: ${err.message}`);
     });
 });
 
-// Coloca o servidor para rodar na porta 666
 servidor.listen(666, () => {
     console.log('=========================================');
     console.log('🛡️ Dungeons&Sockets - Mestre da Mesa ON');
